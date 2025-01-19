76f76ac6e6acacf0a15e67955892c19624fa39d4
arincdemir <68223368+arincdemir@users.noreply.github.com>
2025-01-19 12:28:16 +0000

    Deploying to gh-pages from @ arincdemir/chord_transposer_rust@fb9d726ceae6255b4306dbe4d7d20e64cfe1bc22 🚀

---
+++ sw.js
@@ -0,0 +1,26 @@
+var cacheName = 'egui-template-pwa';
+var filesToCache = [
+  './',
+  './index.html',
+  './chord_transposer_rust.js',
+  './chord_transposer_rust_bg.wasm',
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function (e) {
+  //self.skipWaiting(); // Forces the new service worker to activate immediately
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
